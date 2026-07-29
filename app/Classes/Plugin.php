1f9f942ebd2ac4b648247bec247411cad079697f
nescryo <muhammadryo15@gmail.com>
2026-07-29 10:14:44 +0800

    wip

--- app/Classes/Plugin.php
+++ app/Classes/Plugin.php
@@ -156,7 +156,15 @@ abstract class Plugin implements HasPlugin
 
             // Create target symlink public plugins assets directory if required
             if (! file_exists($publicPluginAssetPath)) {
-                File::relativeLink($pluginAssetPath, rtrim($publicPluginAssetPath, '/'));
+                try {
+                    File::relativeLink($pluginAssetPath, rtrim($publicPluginAssetPath, '/'));
+                } catch (\Throwable $th) {
+                    try {
+                        File::copyDirectory($pluginAssetPath, $publicPluginAssetPath);
+                    } catch (\Throwable $e) {
+                        Log::warning('Failed to copy public plugin asset directory: '.$publicPluginAssetPath);
+                    }
+                }
             }
         }
     }
